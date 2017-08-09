fc88d6e8b9068c0cdeb041bf8bac10405b618940
Nitesh <nickydesai.mail@gmail.com>
2017-08-08 21:59:58 -0500

    Adds buttons for play and reset

--- app/src/main/java/com/example/nicky/timerpractice/MainActivity.java
+++ app/src/main/java/com/example/nicky/timerpractice/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.nicky.timerpractice;
 
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.CountDownTimer;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,26 +23,49 @@ public class MainActivity extends AppCompatActivity {
     NotificationCompat.Builder mBuilder;
     private final int NOTIFICATION_ID = 1;
     static ArrayList<Integer> timesArray;
+    static int timerPos;
+    public static int timeElapsed;
     TextView textView;
+    boolean isRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.test_view1);
+        final Button playButton = (Button) findViewById(R.id.button_play_pause);
+
+        isRunning = false;
+        timerPos = 0;
+        timeElapsed = 0;
 
         timesArray = new ArrayList<>();
         timesArray.add(3);
         timesArray.add(5);
         timesArray.add(7);
-        timerFunction(timesArray.get(0));
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning) {
+                    playButton.setText("play");
+                    isRunning = false;
+                } else {
+                    playButton.setText("pause");
+                    timerFunction(timerPos, 0);
+                    isRunning = true;
+                }
+
+            }
+        });
     }
 
-    public void timerFunction(final Integer sec) {
-        textView.setText(sec.toString());
-        timesArray.remove(0);
 
-        CountDownTimer countDownTimer = new CountDownTimer(sec * 1000, 1000) {
+    public void timerFunction(final int timerPos, final int timeElapsed) {
+
+        int countdownTime = (timesArray.get(timerPos) * 1000) - timeElapsed;
+
+        CountDownTimer countDownTimer = new CountDownTimer(countdownTime, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 textView.setText("seconds remaining: " + millisUntilFinished / 1000);
@@ -53,10 +74,10 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                Log.v("***",timesArray.size() + " ");
+                Log.v("***", timesArray.size() + " ");
                 if (timesArray.size() > 0) {
                     Toast.makeText(getApplicationContext(), "Ding", Toast.LENGTH_SHORT).show();
-                    timerFunction(timesArray.get(0));
+                    timerFunction(timerPos, 0);
                 } else {
                     Toast.makeText(getApplicationContext(), "Ding, Ding Ding!!!", Toast.LENGTH_SHORT).show();
                 }
@@ -66,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
 
         countDownTimer.start();
     }
-
 
 
 }
