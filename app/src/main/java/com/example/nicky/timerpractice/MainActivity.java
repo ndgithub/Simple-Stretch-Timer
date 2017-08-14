a3ee18382860dce24c857543788d60f660e88309
Nitesh <nickydesai.mail@gmail.com>
2017-08-13 20:01:29 -0500

    Improves functionality of timer

--- app/src/main/java/com/example/nicky/timerpractice/MainActivity.java
+++ app/src/main/java/com/example/nicky/timerpractice/MainActivity.java
@@ -1,95 +1,147 @@
 package com.example.nicky.timerpractice;
 
-import android.app.NotificationManager;
-import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
 
-    NotificationManager mNotificationManager;
-    NotificationCompat.Builder mBuilder;
-    private final int NOTIFICATION_ID = 1;
     static ArrayList<Integer> timesArray;
-    static int timerPos;
-    public static int timeElapsed;
+    private int timerPos;
+    public static long timeElapsed;
+    public long startingTime;
     TextView textView;
     boolean isRunning;
+    MyCountdownTimer countDownTimer;
+    Button playButton;
+    Button resetButton;
+    private final int TICK_INTERVAL = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.test_view1);
-        final Button playButton = (Button) findViewById(R.id.button_play_pause);
 
-        isRunning = false;
-        timerPos = 0;
-        timeElapsed = 0;
+        textView = (TextView) findViewById(R.id.test_view1);
+
+        playButton = (Button) findViewById(R.id.button_play_pause);
+        resetButton = (Button) findViewById(R.id.button_reset);
 
         timesArray = new ArrayList<>();
         timesArray.add(3);
         timesArray.add(5);
         timesArray.add(7);
 
+        reset();
+
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isRunning) {
-                    playButton.setText("play");
-                    isRunning = false;
+                if (!isRunning) {
+                    play();
                 } else {
-                    playButton.setText("pause");
-                    timerFunction(timerPos, 0);
-                    isRunning = true;
+                    pause();
                 }
-
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
             }
         });
     }
 
+    private void play() {
+        playButton.setText("pause");
 
-    public void timerFunction(final int timerPos, final int timeElapsed) {
+        startTimer(returnCountdownTime());
+        isRunning = true;
+    }
 
-        int countdownTime = (timesArray.get(timerPos) * 1000) - timeElapsed;
+    private void pause() {
+        timeElapsed = (SystemClock.elapsedRealtime() - startingTime) + timeElapsed;
+        playButton.setText("play");
+        countDownTimer.cancel();
+        isRunning = false;
+    }
 
-        CountDownTimer countDownTimer = new CountDownTimer(countdownTime, 1000) {
+    private void reset() {
+        timerPos = 0;
+        timeElapsed = 0;
+        textView.setText(timesArray.get(0) + " ");
+
+        if (isRunning) {
+            countDownTimer.cancel();
+            startTimer(returnCountdownTime());
+        }
+
+    }
+
+    private void startTimer(long countdownTime) {
+        long leftover = countdownTime % TICK_INTERVAL;
+        final Handler handler = new Handler();
+        startingTime = SystemClock.elapsedRealtime();
+        countDownTimer = new MyCountdownTimer(countdownTime - leftover, TICK_INTERVAL) {
             @Override
             public void onTick(long millisUntilFinished) {
-                textView.setText("seconds remaining: " + millisUntilFinished / 1000);
-
+                textView.setText("seconds remaining: " + Math.ceil(millisUntilFinished / 1000.));
+                Log.v("***", "millisUntilFinished: " + millisUntilFinished );
             }
 
             @Override
             public void onFinish() {
-                Log.v("***", timesArray.size() + " ");
-                if (timesArray.size() > 0) {
-                    Toast.makeText(getApplicationContext(), "Ding", Toast.LENGTH_SHORT).show();
-                    timerFunction(timerPos, 0);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Ding, Ding Ding!!!", Toast.LENGTH_SHORT).show();
-                }
-
+                textView.setText("caca");
+                timerFinished();
             }
         };
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                countDownTimer.start();
+            }
+        }, leftover);
 
-        countDownTimer.start();
     }
 
+    private long returnCountdownTime() {
+        return (timesArray.get(timerPos) * 1000) - timeElapsed;
+    }
 
+    private void timerFinished() {
+        timerPos++;
+        if (isStretchesRemaining()) {
+            timeElapsed = 0;
+            startTimer(returnCountdownTime());
+            Toast.makeText(getApplicationContext(), "Ding", Toast.LENGTH_SHORT).show();
+        } else {
+            pause();
+            reset();
+            Toast.makeText(getApplicationContext(), "Ding, Ding Ding!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isStretchesRemaining() {
+        return timerPos < timesArray.size();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
+
+//TODO: Handle orientation change
+
+
 
 
 
